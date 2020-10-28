2cf4c655ad1761ea104c615182974f4ac96b2fbb
dmitrii <dmitry.esin@gmail.com>
2020-10-28 19:58:29 +0300

    close add fake todo feature#2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/assets/todos.ts
@@ -0,0 +1,14 @@
+export const todos = [
+  {
+    "id": "0163e77e-22df-4493-a6dc-f62c473a2eef",
+    "text": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, aperiam."
+  },
+  {
+    "id": "4b5496f4-c9e5-4011-8ef2-59cd74f3f1d2",
+    "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facilis adipisci repellendus rerum non! Eius aut repellendus pariatur enim temporibus deserunt reprehenderit doloribus unde distinctio minus corrupti repudiandae ipsa consequatur, voluptate expedita totam laudantium culpa incidunt neque quam amet. Exercitationem saepe consequuntur accusamus pariatur repellat aliquam odio officiis, velit aperiam."
+  },
+  {
+    "id": "2bd0528e-319a-4b04-88ae-9f8bf2eef491",
+    "text": ""
+  }
+]
